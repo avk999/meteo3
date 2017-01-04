18ee2c042d181b95279c66213716f4a567439f2c
avk999 <avk2@kovalev-nb-w7.yamoney.ru>
2017-01-04 20:46:50 +0100

    Add DHT11

--- meteo3.ino
+++ meteo3.ino
@@ -17,6 +17,15 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP085_U.h>
 
+// DHT11
+#include <DHT.h>
+// #include <DHT_U.h>
+#define DHTPIN 14 
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE, 15); //11 is some magic value for ESP8266
+
+
+
 // MQTT
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
@@ -48,6 +57,9 @@ WiFiClient wifi;
 Adafruit_MQTT_Client mqtt(&wifi, MQTT_HOST, MQTT_PORT, MQTT_LOGIN, MQTT_PASS);
 Adafruit_MQTT_Publish bmptemp = Adafruit_MQTT_Publish(&mqtt, TOPIC_PREFIX "temperature/bmp");
 Adafruit_MQTT_Publish bmppressure = Adafruit_MQTT_Publish(&mqtt, TOPIC_PREFIX "pressure/bmp");
+Adafruit_MQTT_Publish dhttemp = Adafruit_MQTT_Publish(&mqtt, TOPIC_PREFIX "temperature/dht");
+Adafruit_MQTT_Publish dhthum = Adafruit_MQTT_Publish(&mqtt, TOPIC_PREFIX "humidity/dht");
+
 
 
 void MQTT_connect() {
@@ -138,9 +150,15 @@ void setup() {
     } else {
       u8g2.print("BMP init failed!");
     }
-    u8g2.setCursor(0,61);
-    u8g2.print("exiting setup()");
     u8g2.sendBuffer();
+    u8g2.setCursor(0,61);
+
+    dht.begin();
+    u8g2.print("DHT11 init done");
+    u8g2.sendBuffer();
+   // u8g2.setCursor(0,76);
+   // u8g2.print("setup() done");
+   // u8g2.sendBuffer();
  
     delay(1000);
 
@@ -195,12 +213,29 @@ void loop() {
  //} else {
 //  u8g2.print("temp fail");
 // }
+
  bmptemp.publish(temperature);
  bmppressure.publish(event.pressure);
  
 
 
  u8g2.sendBuffer();
+
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+  u8g2.setCursor(0,61);
+  if  (isnan(h) || isnan(t)){
+    u8g2.print("DHT error, reboot");
+    delay(2000);
+    ESP.restart();
+  }
+  dhttemp.publish(t);
+  dhthum.publish(h);
+  u8g2.print(h,1);
+  u8g2.print(" %");
+  u8g2.sendBuffer();
+ 
  delay(30000);
   
 }
